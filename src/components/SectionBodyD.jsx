34996ec89dd6cd569ec3caf13cc9e1eca125bc08
R-I-F <ibrahimrefaeei@gmail.com>
2024-05-15 16:26:33 +0300

    fix: sectionD conditional submit rules

--- src/components/SectionBodyD.jsx
+++ src/components/SectionBodyD.jsx
@@ -28,18 +28,46 @@ export default function SectionBodyD({name}){
     const handleSubmit = (event) => {
         setIsBtnClicked(true);
         event.preventDefault();
-        // Process form data here (e.g., send it to a server)
-        console.log(formData);
-        writeMessageToDb(formData.name, formData.email, formData.message)
-        // Optionally, reset form fields after submission
+        // writeMessageToDb(formData.name, formData.email, formData.message)
+ 
         setTimeout(()=>{
-            setFormData({
-                name: '', 
-                email: '',
-                message: ''
-            });
+            // setFormData({
+            //     name: '', 
+            //     email: '',
+            //     message: ''
+            // });
             setIsBtnClicked(false);
+            setIsBtnHovered(false);
         },1000)
+        /*
+        1. check if name field filled 
+            true: check if email filled 
+            false promp user to fill email
+            true: check if email field contains @
+            false: prompt user that emails must include @
+            true: check if message at least contains 30 characters 
+                    false: prompt user that message should at least contain 30 characters
+                        true send message to database
+                            true: notify user that the message has been sent
+                            false: notify user that a problem happened while sending the message
+        */
+        if(formData.name){
+            if(formData.email){
+                if(formData.email.includes("@")){
+                    if(formData.message.length >= 30){
+                        console.log("all fields are true")
+                        //send data to db and reset button style
+                    }
+                    else {console.log("messages must be larger than 30 characters long")}
+                }
+                else {console.log("email must include @ character")}
+            }
+            else {console.log("please fill out email")}
+        }
+        else {
+            console.log("please fill out name")
+            //display notification
+        }
         
     };
     
@@ -48,14 +76,27 @@ export default function SectionBodyD({name}){
         return;
     }
     
-    function activeClassSetter(x, y){
-        if(x || y){
-            return ('special-btn-clicked');
+    function renderBtn(){
+        if(isBtnClicked || isBtnHovered){
+            return(
+                <>
+                    <div className = 'special-btn-clicked'/>
+                    <p className='btn-p btn-clicked'>SHOOT</p>
+                </>
+            )
         }
-        else return('special-btn-not-clicked');
+        else {
+            return(
+                <>
+                    <div className = 'special-btn-not-clicked'/>
+                    <p className='btn-p'>SHOOT</p>
+                </>
+            )
+        }
     }
     
     // console.log(formData)
+    // console.log(isBtnClicked);
     return(
         <Element
         name = {name}>
@@ -96,10 +137,7 @@ export default function SectionBodyD({name}){
                         onClick={(event)=>handleSubmit(event)}
                         onMouseEnter={()=>handleHover(true)}
                         onMouseLeave={()=>handleHover(false)}>
-                            <div className = {`${activeClassSetter(isBtnClicked, isBtnHovered)}`}/>
-                            <p className={`btn-p ${isBtnClicked || isBtnHovered? 'btn-clicked':''}`}>
-                                SHOOT
-                            </p>
+                            { renderBtn() }
                         </button>
                     </form>
                 </div>
